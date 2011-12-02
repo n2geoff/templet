9d6ce0ded102a4569b38b0b1ccbdee43d20d6dd8
Geoff Doty <n2geoff@gmail.com>
2011-12-01 19:25:21 -0500

    corrected the copyright information

--- templet.php
+++ templet.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 /**
  * Templet - A PHP-Based Template Class
@@ -17,10 +17,6 @@
  * 	* Redistributions in binary form must reproduce the above copyright notice, this list
  * 	  of conditions and the following disclaimer in the documentation and/or other materials
  * 	  provided with the distribution.
- *
- * 	* Neither the name of the SimplePie Team nor the names of its contributors may be used
- * 	  to endorse or promote products derived from this software without specific prior
- * 	  written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
@@ -61,7 +57,7 @@ class Templet {
      * @param mixed $key  
      * @param var $value
      */
-    function __set($key, $value = NULL) 
+    public function __set($key, $value = NULL) 
     {
         //if array recurrsively assign values keys
         if(is_array($key) 
@@ -88,7 +84,7 @@ class Templet {
 	 *
 	 * Removes not set notices
 	 */
-    function __get($key) 
+    public function __get($key) 
     {
 		if(!isset($key))
 		{
